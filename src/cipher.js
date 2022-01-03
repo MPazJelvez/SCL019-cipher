7a5fc615349fa9e7da6f2583d825ef959fcad6e0
María Paz Jélvez Herrera <mpazjelvez@gmail.com>
2022-01-03 13:09:32 -0300

    primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,21 @@
 const cipher = {
-  // ...
+  encode: function(desplazamiento, texto) {
+    if (texto === "" || desplazamiento === 'null' || texto.length === 0 || desplazamiento === 0){
+      throw new TypeError();
+    }
+    const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    desplazamiento = (desplazamiento % 26 + 26) % 26; 
+    return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) + desplazamiento) % 26]);
+    },
+
+  decode: function(desplazamiento,texto) {
+    if (texto === "" || desplazamiento === 'null' || texto.length === 0 || desplazamiento === 0){
+      throw new TypeError();
+    }
+    const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    desplazamiento = (desplazamiento % 26 - 26) % 26; 
+    return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) - desplazamiento) % 26]);
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import cipher from './cipher.js';
+window.addEventListener("load",inicio,true);
+
+function inicio(){ //asignar código a cada elemento (llamarlos)
+    document.getElementById("mensaje").addEventListener("keyup", function(){
+        this.value = this.value.toUpperCase();
+    }, true); //convertir a mayúscula
+    
+    document.getElementById("encrypt").addEventListener("click",function(){  
+        let texto = document.getElementById("mensaje").value;
+        let desplazamiento = document.getElementById("desplazamiento").value;               
+        document.getElementById("mensaje2").value = cipher.encode(desplazamiento, texto);
+    },true); //al hacer click en el boton se obtiene msje y se realiza la función
+    document.getElementById("decrypt").addEventListener("click",function(){  
+        let texto = document.getElementById("mensaje").value;
+        let desplazamiento = document.getElementById("desplazamiento").value;                               
+        document.getElementById("mensaje2").value = cipher.decode(desplazamiento, texto);
+    },true); //al hacer click se obtiene msje y se decodifica
+}
 
 console.log(cipher);
